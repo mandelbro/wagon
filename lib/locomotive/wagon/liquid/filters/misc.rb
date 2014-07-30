5e048ac87ae51fcf124e1bcb7851f229a281bda1
mandelbro <chrismontes@about.me>
2014-07-30 09:55:17 -0700

    Improves darken color method

--- lib/locomotive/wagon/liquid/filters/misc.rb
+++ lib/locomotive/wagon/liquid/filters/misc.rb
@@ -24,10 +24,7 @@ module Locomotive
 
           def darken_color(hex_color, amount=0.4)
             hex_color = hex_color.gsub('#','')
-            rgb = hex_color.scan(/../).map {|color| color.hex}
-            rgb[0] = (rgb[0].to_i * amount).round
-            rgb[1] = (rgb[1].to_i * amount).round
-            rgb[2] = (rgb[2].to_i * amount).round
+            rgb = hex_color.scan(/../).map(&:hex).map{|color| color * amount}.map(&:round)
             "#%02x%02x%02x" % rgb
           end
 
