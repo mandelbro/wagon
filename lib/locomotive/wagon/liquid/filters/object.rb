bab0f57f8eb9179023a40e28064365f54591139a
mandelbro <chrismontes@about.me>
2014-12-03 14:12:38 -0800

    Adds object filters

--- lib/locomotive/wagon/liquid/filters/object.rb
+++ lib/locomotive/wagon/liquid/filters/object.rb
@@ -8,6 +8,36 @@ module Locomotive
             object.reject {|k,v| k == "collections" }.to_json
           end
 
+          def parse_json string
+            JSON.parse(string.to_s)
+          end
+
+          def order_by_date object, field, direction = 'asc'
+
+            object.sort do |a, b|
+              a_date = parse_date_time(a[field] || 0).to_i
+              b_date = parse_date_time(b[field] || 0).to_i
+              if direction == 'asc'
+                a_date <=> b_date
+              else
+                a_date <=> b_date
+              end
+            end
+          end
+
+          def since object, field, time
+            object.select do |item|
+              item_date = parse_date_time(item[field] || 0).to_i
+              item_date >= time.to_i
+            end
+          end
+
+          def only_with object, field
+            object.reject do |item|
+              item[field].empty?
+            end
+          end
+
         end
 
         ::Liquid::Template.register_filter(Object)
